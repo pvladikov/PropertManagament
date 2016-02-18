d97ca899920c21b30e8f5c79960cb4fb0eba8560
pvladikov <pvladikov@hotmail.com>
2016-02-18 21:41:28 +0200

    refactoring

--- PropertyManagament/App_Start/BundleConfig.cs
+++ PropertyManagament/App_Start/BundleConfig.cs
@@ -8,24 +8,45 @@ namespace WebApplication1
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                         "~/Content/bootstrap.css",
+                         "~/Content/bootstrap-theme.css",
+                         "~/Content/bootstrap-theme.css.map",
+                         "~/Content/bootstrap-theme.min.css.map",
+                         "~/Content/bootstrap.css.map",
+                         "~/Content/bootstrap.min.css",
+                         "~/Content/bootstrap.min.css.map"
+             ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(                     
+                      "~/Content/site.css"));      
+
+            bundles.Add(new StyleBundle("~/Content/ng-grid").Include(
+                    "~/Content/ng-grid.css",
+                    "~/Content/ng-grid.min.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+           "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/jquery.validate*"));  
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ng-grid").Include(
+                    "~/Scripts/ng-grid.js",
+                    "~/Scripts/ng-grid.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app-controller").Include(
+                    "~/Scripts/app-controller.js"));
+
         }
+
     }
 }
---
+++ PropertyManagament/Scripts/app-controller.js
@@ -0,0 +1,50 @@
+﻿var app = angular.module('homeApplication', ['ngGrid']);
+
+app.controller('homeController', function ($scope, $http) {
+    $http.get('/property/read').success(function (data) {
+        $scope.propertymanagament = data; 
+        $scope.searchProperty = '';
+    });
+
+     $scope.someProp = 'abc',
+     $scope.editProperty = function () {
+         $http.post('/property/editproperty').success(function (data) {
+             $scope.propertymanagament.push(data);
+         });
+     };
+
+    $scope.gridOptions = {
+        data: 'propertymanagament',
+        columnDefs: [{ field: 'upi', displayName: 'UPI' },
+        { field: 'area', displayName: 'Area' },
+        { name: 'Modify',
+        cellTemplate: '<button type="button" class="btn btn-primary" ng-click="editProperty()">Modify</button>',
+          name: 'Delete',
+          cellTemplate: '<button type="button" class="btn btn-primary" ng-click="editProperty()">Delete</button>'
+        }
+         ]
+    }; 
+
+    //Create New Property
+    $scope.property = function () {
+        $http.post('/property/create').success(function (data) {
+            $scope.propertymanagament.push(data);            
+        });    
+    }
+
+    $scope.editingData = {};
+
+    $scope.modify = function (property) {
+        $scope.editingData[property.id] = true;
+    };
+
+    //$scope.remove = function (dragon) {
+    //    if (confirm('Are you sure?')) {
+    //        $http.post('/service/remove', JSON.stringify(dragon)).success(function (result) {
+    //            if (result) {
+    //                $scope.dragons.splice($scope.dragons.indexOf(dragon), 1);
+    //            }
+    //        });
+    //    }
+    //}
+});
